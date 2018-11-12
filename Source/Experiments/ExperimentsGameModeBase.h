ba7786b24645f9ff03d32d9b4b1833430147a75e
jacksoncougar <jacksoncougar@gmail.com>
2018-11-12 14:57:50 -0700

    Initial commit

---
+++ Source/Experiments/ExperimentsGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ExperimentsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EXPERIMENTS_API AExperimentsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
